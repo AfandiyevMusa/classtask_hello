d2f4bc123813ce2076743fd8cd366301b656c2e2
Afandiyev Musa <MusaHA@code.local>
2023-02-19 15:13:36 +0400

    hello

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+let btnOn = document.querySelector(".on");
+let btnOff = document.querySelector(".off");
+
+btnOn.addEventListener("dblclick", function(){
+    btnOn.classList.add("active")
+    
+})
+
+
+
